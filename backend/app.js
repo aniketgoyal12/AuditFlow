417dcf8f150e084220386ca321a4c4f95dee4b02
Aniket Goyal <143060619+aniketgoyal12@users.noreply.github.com>
2026-04-10 15:00:58 +0530

    cors error fix

--- backend/app.js
+++ backend/app.js
@@ -47,7 +47,10 @@ const createApp = () => {
       crossOriginResourcePolicy: false,
     })
   );
-  app.use(cors(buildCorsOptions()));
+  app.use(cors({
+  origin: "https://audit-flow-lemon.vercel.app",
+  credentials: true
+}));
   app.use(compression());
   app.use(express.json({ limit: "1mb" }));
   app.use(express.urlencoded({ extended: true, limit: "1mb" }));
